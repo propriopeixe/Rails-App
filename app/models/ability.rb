04cafde85dead3f01445e96892ba4c85666f5d9b
propriopeixe <pedroangelo.sj@gmail.com>
2016-02-23 18:41:32 +0000

    modified ability.rb to allow admins to destroy comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.admin?
-      can :manage, User, id: user.id
+      can :destroy, Comment
     else 
       can :read, :all
     end
