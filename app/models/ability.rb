da3ecae832d5fd18d44852204f5fe3b4277c94e3
propriopeixe <pedroangelo.sj@gmail.com>
2016-03-01 20:09:25 +0000

    fixed the user_controller_spec.rb code and the ability.rb code

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,23 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
+
+    alias_action :create, :read, :update, to: :user_actions
+
     if user.admin?
-      can :destroy, Comment
-      can :manage, User
-    else 
-      can :read, :all
+      can :manage, :all
+    else # for both guests and registered users!
+      can [:read, :update], User, id: user.id
+
+      can [:create, :read], Comment
+      can [:update, :destroy], Comment do |comment|
+        comment.user_id == user.id and comment.created_at > 2.hours.ago
+      end
+      can :read, Product
+
+      can :create, Order
+      can :read, Order, user_id: user.id
+      # can :update, Order do  if state == Order.CREATED
     end
     
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,44 +8,41 @@ describe UsersController, :type => :controller do
   end
 
   describe "GET #show" do
-      
-    context "User is logged in" do
-         
-        before do
-            sign_in @user
-            it "loads correct user details" do
-               get :show
-               expect(response).to have_http_status(200)
-               expect(assigns(:user)).to eq @user
-            end
-        end
 
-     end
-     
-    context "Both users are logged in" do
-        
-        before do
-            sign_in @user
-            sign_in @user2
-            
-            it "redirects user to the root" do
-               get :show, id: @user.id
-               expect(response).to redirect_to(root_path)
-            end
-            
-        end
-          
+    context "User is logged in" do
+
+      before do
+        sign_in @user
+      end
+
+      it "loads correct user details" do
+        get :show, id: @user
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq @user
+      end
+    end
+
+    context "User tries to access another user's page" do
+
+      before do
+        sign_in @user
+      end
+
+      it "redirects user to the root" do
+        get :show, id: @user2.id
+        expect(response).to redirect_to(root_path)
+      end
     end
 
     context "No user is logged in" do
-         
-       it "redirects to login" do
-         get :show, id: @user.id
-         expect(response).to redirect_to(user_session_path)
-       end
-       
-     end
-     
+
+      it "redirects to login" do
+        get :show, id: @user.id
+        expect(response).to redirect_to(user_session_path)
+      end
+
+    end
+
   end
 
 end
