5a969fbc9498c4c3d1e1d035f717e933ab464328
propriopeixe <pedroangelo.sj@gmail.com>
2016-03-10 01:14:45 +0000

    instaled brakeman and added a fix to the CVE-2016-0751 issue on the mime_types.rb initializer

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,10 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+
+require 'action_dispatch/http/mime_type'
+
+Mime.const_set :LOOKUP, Hash.new { |h,k|
+  Mime::Type.new(k) unless k.blank?
+} 
